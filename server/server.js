8a0b6edc4ee5cb365de8458d4c53d85586ffc0b7
justin <jmdingeman@gmail.com>
2025-10-23 12:44:47 -0700

    init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`)
+})
